509276dd5e73f6a24f5c0c58240cae06bdb27472
samf2022 <samf0794@gmail.com>
2023-03-15 11:23:51 -0400

    crud (create and delete)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private taskService: TasksService) {}
+
   @Get()
-  helloWorld() {
-    return 'Hello, World!';
+  getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post('create')
+  createTasks(@Body() newTask: CreateTaskDto) {
+    return this.taskService.createTasks(newTask.title, newTask.description);
+  }
+
+  @Delete(':id')
+  deleteTasks(@Param('id') id: string) {
+    this.taskService.deleteTasks(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  //simulate a database
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'first task',
+      description: 'some task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTasks(title: string, description: string) {
+    const task = {
+      id: new Date().toISOString(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+  updateTasks() {}
+
+  deleteTasks(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+}
